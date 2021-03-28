28643bc99df3150d58d17ec573b3bcb1f06cf79e
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-28 16:03:25 +0200

    create 2 interfaces BasicOperations and AdvancedOperations

---
+++ src/main/java/com/assignment02/firstproject/_pass_thibichloanChung/AdvancedOperations.java
@@ -0,0 +1,10 @@
+package com.assignment02.firstproject._pass_thibichloanChung;
+
+public interface AdvancedOperations {
+	public double square(double number);
+	public double absolutValue(double number);
+	public double raisedToThePowerOf(double base, double exponent);
+	public double squareRoot(double number);
+	public double getSign(double number);
+
+}
---
+++ src/main/java/com/assignment02/firstproject/_pass_thibichloanChung/BasicOperations.java
@@ -0,0 +1,9 @@
+package com.assignment02.firstproject._pass_thibichloanChung;
+
+public interface BasicOperations {
+	public double addition(double firstNumber, double secondNumber);
+	public double subtraction(double firstNumber, double secondNumber);
+	public double multiplication(double firstNumber, double secondNumber);
+	public double division(double firstNumber, double secondNumber);
+
+}
