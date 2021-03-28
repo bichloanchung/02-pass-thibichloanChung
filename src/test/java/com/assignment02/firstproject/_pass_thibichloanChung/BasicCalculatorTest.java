9b5a091acd19c9698355c67989f56f71f1a6e890
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-28 19:17:03 +0200

    start test with BasicCalculator

--- src/test/java/com/assignment02/firstproject/_pass_thibichloanChung/BasicCalculatorTest.java
+++ src/test/java/com/assignment02/firstproject/_pass_thibichloanChung/BasicCalculatorTest.java
@@ -2,14 +2,39 @@ package com.assignment02.firstproject._pass_thibichloanChung;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 public class BasicCalculatorTest {
 
 	@Test
-	public void testAdditionZero() {
-		
-		
+	public void testAddZero() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		//assertEquals(bcal.addition(0.0, 0.0),0,0);
+		assertEquals(bcal.addition(x, 0),x,0);	
+	}
+	
+	@Test
+	public void testAddPosNumbers() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		double y = rd.nextDouble();
+	
+		assertEquals(bcal.addition(x, y), x+y ,0);	
+	}
+	
+	@Test
+	public void testAddNegNumbers() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble()* (-1);
+		double y = rd.nextDouble()* (-1);
+	
+		assertEquals(bcal.addition(x, y), x+y ,0);	
 	}
 
 }
