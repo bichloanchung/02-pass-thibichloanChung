400694b35df3a12baf1ee7742c45721edd64d813
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-28 20:32:14 +0200

    update test for BasicCalculatorTest

--- src/test/java/com/assignment02/firstproject/_pass_thibichloanChung/BasicCalculatorTest.java
+++ src/test/java/com/assignment02/firstproject/_pass_thibichloanChung/BasicCalculatorTest.java
@@ -14,27 +14,134 @@ public class BasicCalculatorTest {
 		Random rd = new Random();
 		double x = rd.nextDouble();
 		//assertEquals(bcal.addition(0.0, 0.0),0,0);
-		assertEquals(bcal.addition(x, 0),x,0);	
+		assertEquals(bcal.addition(x, 0), x ,0);	
 	}
 	
 	@Test
-	public void testAddPosNumbers() {
+	public void testAddPositiveNbrs() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		double y = rd.nextDouble();
+		
+		assertEquals(bcal.addition(x, y), (x+y) ,0);	
+	}
+	
+	@Test
+	public void testAddNegativeNbrs() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble()* (-1);
+		double y = rd.nextDouble()* (-1);
+		
+		assertEquals(bcal.addition(x, y), (x+y) ,0);	
+	}
+	
+	@Test
+	public void testAddNegPosN() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble()* (-1);
+		double y = rd.nextDouble();
+	
+		assertEquals(bcal.addition(x, y), (x+y) ,0);	
+	}
+	
+	@Test
+	public void testSubZero() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+	
+		assertEquals(bcal.subtraction(x, 0), x ,0);	
+	}
+	@Test
+	public void testSubPositiveNbrs() {
 		BasicCalculator bcal = new BasicCalculator();
 		Random rd = new Random();
 		double x = rd.nextDouble();
 		double y = rd.nextDouble();
 	
-		assertEquals(bcal.addition(x, y), x+y ,0);	
+		assertEquals(bcal.subtraction(x, y), (x-y) ,0);	
 	}
 	
 	@Test
-	public void testAddNegNumbers() {
+	public void testSubNegativeNbrs() {
 		BasicCalculator bcal = new BasicCalculator();
 		Random rd = new Random();
 		double x = rd.nextDouble()* (-1);
 		double y = rd.nextDouble()* (-1);
 	
-		assertEquals(bcal.addition(x, y), x+y ,0);	
+		assertEquals(bcal.subtraction(x, y), (x-y) ,0);	
 	}
+	
+	@Test
+	public void testSubNegPosN() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		double y = rd.nextDouble()* (-1);
+	
+		assertEquals(bcal.subtraction(x, y), (x-y) ,0);	
+	}
+	
+	@Test
+	public void testMultiZero() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+	
+		assertEquals(bcal.multiplication(x, 0), 0 ,0);	
+	}
+	
+	@Test
+	public void testMultiPositiveNbrs() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		double y = rd.nextDouble();
+	
+		assertEquals(bcal.multiplication(x, y), (x*y) ,0);	
+	}
+	
+	@Test
+	public void testMultiNegativeNbrs() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		double y = rd.nextDouble()* (-1);
+	
+		assertEquals(bcal.multiplication(x, y), (x*y) ,0);	
+	}
+	
+	@Test
+	public void testDivZero() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+	
+		assertEquals(bcal.division(x, 0), 1e10, 0);	
+	}
+	
+	@Test
+	public void testDivPositiveNbrs() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		double y = rd.nextDouble();
+	
+		assertEquals(bcal.division(x, y), (x/y), 0);	
+	}
+	@Test
+	public void testDivNegativNbrs() {
+		BasicCalculator bcal = new BasicCalculator();
+		Random rd = new Random();
+		double x = rd.nextDouble();
+		double y = rd.nextDouble()* (-1);
+	
+		assertEquals(bcal.division(x, y), (x/y), 0);	
+	}
+	
+	
 
 }
