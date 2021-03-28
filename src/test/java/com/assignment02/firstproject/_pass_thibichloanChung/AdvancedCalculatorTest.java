35a2a4535bdbcba55b4d68470f41c22bd9efc016
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-28 22:37:47 +0200

    begin test methods of AdvancedCalculatorTest

--- src/test/java/com/assignment02/firstproject/_pass_thibichloanChung/AdvancedCalculatorTest.java
+++ src/test/java/com/assignment02/firstproject/_pass_thibichloanChung/AdvancedCalculatorTest.java
@@ -2,13 +2,35 @@ package com.assignment02.firstproject._pass_thibichloanChung;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 public class AdvancedCalculatorTest {
+	AdvancedCalculator acal = new AdvancedCalculator();
+	Random rd = new Random();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSquareZero() {
+		double x = 0;
+		
+		assertEquals(acal.square(x), 0 , 0);
+	}
+	
+	@Test
+	public void testSquarePositiveNum() {
+		double x = rd.nextDouble();
+		double result = x * x;
+		
+		assertEquals(acal.square(x), result , 0);
+	}
+	
+	@Test
+	public void testSquarePositiveNum_1() {
+		double x = rd.nextDouble();
+		double result = x * x;
+		
+		assertTrue(acal.square(x) == result);
 	}
 
 }
