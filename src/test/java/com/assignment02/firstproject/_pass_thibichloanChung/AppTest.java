9dc850ffa2fdb14998a53842deab25169f801c9a
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-28 15:49:02 +0200

    create assignment pass project

---
+++ src/test/java/com/assignment02/firstproject/_pass_thibichloanChung/AppTest.java
@@ -0,0 +1,20 @@
+package com.assignment02.firstproject._pass_thibichloanChung;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
