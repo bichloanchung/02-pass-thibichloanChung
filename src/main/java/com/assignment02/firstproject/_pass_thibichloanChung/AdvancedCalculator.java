bb35a3c099f931099c7740b8e9578a673499f63f
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-28 16:51:03 +0200

    implement methods for class AdvancedCalculator

--- src/main/java/com/assignment02/firstproject/_pass_thibichloanChung/AdvancedCalculator.java
+++ src/main/java/com/assignment02/firstproject/_pass_thibichloanChung/AdvancedCalculator.java
@@ -1,5 +1,30 @@
 package com.assignment02.firstproject._pass_thibichloanChung;
 
-public class AdvancedCalculator {
+public class AdvancedCalculator extends BasicCalculator implements AdvancedOperations{
+
+	public double square(double number) {
+		double result = number * number;
+		return result;
+	}
+
+	public double absolutValue(double number) {
+		double result = Math.abs(number);
+		return result;
+	}
+
+	public double raisedToThePowerOf(double base, double exponent) {
+		double result = Math.pow(base, exponent);
+		return result;
+	}
+
+	public double squareRoot(double number) {
+		double result = Math.sqrt(number);
+		return result;
+	}
+
+	public double getSign(double number) {
+		double result = Math.signum(number);
+		return result;
+	}
 
 }
