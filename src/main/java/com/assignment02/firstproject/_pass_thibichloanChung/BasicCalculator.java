981988b58783234d39ac4b8839ca06e21d62c25b
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-28 16:34:05 +0200

    implement methods for class BasicCalculator

--- src/main/java/com/assignment02/firstproject/_pass_thibichloanChung/BasicCalculator.java
+++ src/main/java/com/assignment02/firstproject/_pass_thibichloanChung/BasicCalculator.java
@@ -1,5 +1,31 @@
 package com.assignment02.firstproject._pass_thibichloanChung;
 
-public class BasicCalculator {
+public class BasicCalculator implements BasicOperations{
+
+	public double addition(double firstNumber, double secondNumber) {
+		double result = firstNumber + secondNumber;
+		return result;
+	}
+
+	public double subtraction(double firstNumber, double secondNumber) {
+		double result = firstNumber - secondNumber;
+		return result;
+	}
+
+	public double multiplication(double firstNumber, double secondNumber) {
+		double result = firstNumber * secondNumber;
+		return result;
+	}
+
+	public double division(double firstNumber, double secondNumber) {
+		double result = firstNumber / secondNumber;
+		if (Double.isInfinite(result)) {
+			System.out.println("Division by zero?!");
+			return -1000;
+		} else {
+			return result;
+		}
+	}
+	
 
 }
